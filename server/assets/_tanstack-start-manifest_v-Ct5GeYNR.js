969f6355c658bc3b63fa28a2ea34c62d748a6aac
Naeim Najafvand <naeim4naeim@gmail.com>
2026-05-27 00:20:48 +0200

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Ct5GeYNR.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/vscode/pro/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DWwilGFS.js"] }, "/": { filePath: "D:/vscode/pro/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-COUrg1fc.js"] } }, clientEntry: "/assets/index-DWwilGFS.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-4QjE3BI0.js";
+import "node:events";
+export {
+  w as default
+};
